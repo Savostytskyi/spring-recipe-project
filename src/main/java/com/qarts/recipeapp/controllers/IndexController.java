73270d3404e428a96c6d3ab6ac0d246806409fd8
Savostytskyi <savostytskyi.anton@gmail.com>
2020-07-10 11:57:21 +0200

    added Recipe and Notes entities

--- src/main/java/com/qarts/recipeapp/controllers/IndexController.java
+++ src/main/java/com/qarts/recipeapp/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        System.out.println("Some info 5555");
         return "index";
     }
 }
